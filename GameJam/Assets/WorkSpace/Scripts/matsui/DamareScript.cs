209942600aca1cc2cc0d0f9aa5b3b2c8215dbd74
matsui <s.misan117@gmail.com>
2026-09-08 14:33:23 +0900

    減衰実装

--- GameJam/Assets/WorkSpace/Scripts/matsui/DamareScript.cs
+++ GameJam/Assets/WorkSpace/Scripts/matsui/DamareScript.cs
@@ -4,12 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class DamareScript : MonoBehaviour
 {
-    [SerializeField]int damage = 30;
-    public int kari = 50;
+    public int damage = 1;
+    //public int kari = 50;
     [SerializeField] GameObject cameraObject;
     [SerializeField] GameObject CutInObject;
-    int count = 0;
-    bool isStudent = true;
     public gameManager gameManager;
     public StudentUpDownScript upDown;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,23 +19,12 @@ public class DamareScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isStudent && Keyboard.current.enterKey.wasPressedThisFrame)
+        if (upDown.Speaking && Keyboard.current.enterKey.wasPressedThisFrame)
         {
-            //isStudent = false;
-            //cameraObject.transform.Rotate(50, 180, 0);
-            if(kari - damage > 0)
-            {
-                kari = kari - damage;
-                damage = damage - 2 * count;
-            }
-            else
-            {
-                kari = 0;
-            }
-            count++;
             Instantiate(CutInObject, new Vector3(0, 12.5f, 10.5f), Quaternion.Euler(25, 0, 0));
+
+            gameManager.upDown.Mute();
             gameManager.Add();
-            upDown.Mute();
         }
     }
 }
